0cc0fa802aa7d411b3088be2ae0361da7a640580
HimaMarey <ibrahimmar3y.ad@gmail.com>
2023-06-19 19:11:02 +0300

    create notes list

---
+++ lib/views/widgets/note_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 3,
+      itemBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: CustomNoteItem(),
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/custom_note_item.dart';
+import 'package:notes_app/views/widgets/note_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
@@ -8,13 +9,17 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
-    return   Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
-          SizedBox(height: height *.05,),
-          const CustomAppBar(),
-          CustomNoteItem(),
+          SizedBox(
+            height: height * .05,
+          ),
+           const CustomAppBar(),
+          const Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
