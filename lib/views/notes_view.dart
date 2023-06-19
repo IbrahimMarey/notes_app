33625de3db0e6bfcc9179a0b820fe11724cb10b5
HimaMarey <ibrahimmar3y.ad@gmail.com>
2023-06-19 17:48:50 +0300

    change theme

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
